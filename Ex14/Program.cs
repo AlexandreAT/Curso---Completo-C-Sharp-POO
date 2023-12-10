2bd588a5a5c9967dc7c24cffb720276af2dca208
Alexandre Arribamar <alexandre.arribamar@gmail.com>
2023-12-10 01:05:26 -0300

    Atualizando os exercicios feitos

---
+++ Ex14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex14 {
+    class Program{
+
+        static void Main(string[] args){
+
+            int n, cod;
+            HashSet<int> students = new HashSet<int>();
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("=============================================");
+            Console.WriteLine("EX14 - BUSCANDO O TOTAL DE ALUNOS EM UM CURSO");
+            Console.WriteLine("=============================================");
+
+            Console.Write("Digite quantos alunos tem no curso A: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++){
+                Console.Write($"Digite o código do aluno {i}: ");
+                students.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+
+            Console.Write("Digite quantos alunos tem no curso B: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++){
+                Console.Write($"Digite o código do aluno {i}: ");
+                students.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+
+            Console.Write("Digite quantos alunos tem no curso C: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++){
+                Console.Write($"Digite o código do aluno {i}: ");
+                students.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Total de estudantes: "+students.Count);
+            Console.WriteLine("");
+            Console.WriteLine("Clique enter para voltar ao menu");
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
---
+++ Ex15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex15 {
+    class Program{
+
+        static void Main(string[] args){
+
+            string path = @"C:\Users\alexa\Desktop\Cursos e derivados\Udemy\Curso - C# Completo POO\Ex15\";
+            string fileName, candidateName;
+            int votes;
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("=============================================");
+            Console.WriteLine("EX14 - CALCULANDO OS VOTOS EM URNAS DIFERENTES");
+            Console.WriteLine("=============================================");
+
+            Console.Write("Digite o nome do arquivo que contem os votos (ex: Votes.txt): ");
+            fileName = Console.ReadLine();
+            path += @fileName;
+
+            using(StreamReader sr = new StreamReader(path)){
+                
+                while(!sr.EndOfStream){
+                    string[] vect = sr.ReadLine().Split(',');
+                    candidateName = vect[0];
+                    votes = int.Parse(vect[1]);
+                    if(candidates.ContainsKey(candidateName)){
+                        candidates[candidateName] += votes;
+                    }
+                    else{
+                        candidates.Add(candidateName, votes);
+                    }
+                }
+            }
+            Console.WriteLine();
+            foreach (KeyValuePair<string, int> candidate in candidates)
+            {
+
+                Console.WriteLine(candidate.Key+": "+candidate.Value);
+                
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Clique enter para voltar ao menu");
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
