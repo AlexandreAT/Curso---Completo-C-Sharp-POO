20d3a7dc6870166600d532262b23ef1c5b0e5eb6
Alexandre Arribamar <alexandre.arribamar@gmail.com>
2023-12-12 23:51:46 -0300

    Atualizando os exercicios feitos

---
+++ Projeto Sistema Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Sistema_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
