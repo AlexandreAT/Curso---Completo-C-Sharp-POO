57ebd4d5aa4c17e50ce555579bdb4473c7b29881
Alexandre Arribamar <alexandre.arribamar@gmail.com>
2023-12-16 19:33:13 -0300

    Termindo o CRUD da tabela Seller

---
+++ Projeto Sistema Web/SalesWebMvc/SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : AppDomainUnloadedException
+    {
+
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
